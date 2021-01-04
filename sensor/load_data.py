54ae4ca6182ec17497bf467bcd9bb728561cc231
mirandaliukkkk <61069707+mirandaliukkkk@users.noreply.github.com>
2021-01-04 16:45:00 +0000

    module1

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -8,7 +8,7 @@ def load_sensor_data():
     for sensor_file in sensor_files:
         with open(sensor_file) as data_file:
             data_reader = csv.DictReader(data_file, delimiter=',')
-            for row in dat_file:
+            for row in data_file:
                 sensor_data.append(row)
     return sensor_data
 
